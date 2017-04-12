b754b56bcefde744a75a5da5adbc156f5102b95e
UKeeySDis <799188765@qq.com>
2017-04-12 21:11:31 +0800

    测试debug功能

--- init/main.c
+++ init/main.c
@@ -5,15 +5,17 @@
   > Created Time: 2017年04月11日 星期二 21时42分47秒
  ************************************************************************/
 
-#include "types.h"
 #include "console.h"
+#include "debug.h"
 int kern_entry()
 {
 	
-	
+	init_debug();
+
 	console_clear();
 
-	console_write_color("Hello OS World\n", rc_black, rc_red);
+	printk_color(rc_black, rc_red, "hello os\n");
 
+	panic("test");
 	return 0;
 }
