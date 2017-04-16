4da4db0eb92ccc197220c352421078eea26abd19
UKeeySDis <799188765@qq.com>
2017-04-16 23:25:23 +0800

    新增于时间中断相关的头文件

---
+++ include/timer.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: timer.h
+    > Author: Ukey
+    > Mail: gsl110809@gmail.com
+    > Created Time: 2017年04月16日 星期日 23时06分18秒
+ ************************************************************************/
+
+#ifndef INCLUDE_TIMER_H_
+#define INCLUDE_TIMER_H_
+
+#include "types.h"
+
+void init_timer(uint32_t frequency);
+
+#endif  
