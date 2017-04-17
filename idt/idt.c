0bb2b89c14ba4744b791d0ccb9412a46d73659ca
UKeeySDis <799188765@qq.com>
2017-04-17 14:16:59 +0800

    调整部分代码

--- idt/idt.c
+++ idt/idt.c
@@ -18,11 +18,12 @@ typedef void (*deal_func_t)();
 static deal_func_t deal_func[INTERRUPT_MAX] = {&isr0 , &isr1 , &isr2 , &isr3 , &isr4 , &isr5 , 
 												 &isr6 , &isr7 , &isr8 , &isr9 , &isr10, &isr11,
 												 &isr12, &isr13, &isr14, &isr15, &isr16, &isr17,
-												 &isr19, &isr20, &isr21, &isr22, &isr23, &isr24,
-												 &isr25, &isr26, &isr27, &isr28, &isr29, &isr30,
-												 &isr31, &irq0 , &irq1 , &irq2 , &irq3 , &irq4 ,
-												 &irq5 , &irq6 , &irq7 , &irq8 , &irq9 , &irq10, 
-												 &irq11, &irq12, &irq13, &irq14, &irq15};
+												 &isr18, &isr19, &isr20, &isr21, &isr22, &isr23,
+
+												 &isr24, &isr25, &isr26, &isr27, &isr28, &isr29,
+												 &isr30, &isr31, &irq0 , &irq1 , &irq2 , &irq3 ,
+												 &irq4 , &irq5 , &irq6 , &irq7 , &irq8 , &irq9 , 
+												 &irq10, &irq11, &irq12, &irq13, &irq14, &irq15};
 
 // 设置中断描述符的函数
 static void idt_set_gate(uint8_t num, uint32_t offset, uint16_t selector, uint8_t flags);
@@ -49,10 +50,8 @@ void init_idt()
 	{
 		idt_set_gate(i, (uint32_t)deal_func[i], 0x08, 0x8E);
 	}
-	// 128 将来用于实现系统调用
 	idt_set_gate(128, (uint32_t)isr128, 0x08, 0x8E);
 
-	// 更新设置中断描述符表
 	idt_flush((uint32_t)&idt_ptr);
 }
 
@@ -73,7 +72,7 @@ static void idt_set_gate(uint8_t num, uint32_t offset, uint16_t selector, uint8_
 // 中断处理函数
 void isr_handler(pt_regs_t *regs)
 {
-	if (interrupt_handlers[regs->int_num]) 
+	if (interrupt_handlers[regs->int_num])	
 	{
 	      interrupt_handlers[regs->int_num](regs);
 	}
@@ -90,7 +89,7 @@ void irq_handler(pt_regs_t *regs)
 	if(regs->int_num >= 40)
 	{
 		outb(0xA0, 0x20);
-	}
+	}	
 	outb(0x20, 0x20);
 	if(interrupt_handlers[regs->int_num])
 	{
