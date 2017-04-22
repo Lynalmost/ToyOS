6d3448c8addb644330a252eebbf03b4c2efd24d3
UKeeySDis <799188765@qq.com>
2017-04-22 23:10:50 +0800

    增加物理内存分页测试

--- init/main.c
+++ init/main.c
@@ -9,9 +9,11 @@
 #include "debug.h"
 #include "idt.h"
 #include "timer.h"
+#include "pmm.h"
+
 int kern_entry()
 {
-	
+
 	init_debug();
 	init_gdt();
 	init_idt();
@@ -21,6 +23,11 @@ int kern_entry()
 	printk_color(rc_black, rc_red, "hello os\n");
 
 	init_timer(200);
-	asm volatile ("sti");
+
+	printk("kernel in memory start: 0x%08X\n", kern_start);
+	printk("kernel in memory end:   0x%08X\n", kern_end);
+	printk("kernel in memory used:   %d KB\n\n", (kern_end - kern_start + 1023) / 1024);
+
+	show_memory_map();
 	return 0;
 }
